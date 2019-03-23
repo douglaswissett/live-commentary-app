3ef827357fa592420656c992aa3a6bafc3376f27
Douglas Wissett Walker <douglas.wissett@gmail.com>
2019-03-23 14:20:29 +0000

    ESLint configured

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+    "standard",
+        "airbnb",
+        'plugin:react/recommended',  // Uses the recommended rules from @eslint-plugin-react
+        'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser":  '@typescript-eslint/parser',
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
