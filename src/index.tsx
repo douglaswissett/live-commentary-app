2366c7a759750a9bbc2b26cfe257d05b921ca5cd
Douglas Wissett Walker <douglas.wissett@gmail.com>
2019-03-23 14:57:56 +0000

    Importing Container in src/index and added title

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,14 @@
-/**
- * @format
- */
-
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+import React, { PureComponent } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Container } from './components';
 
 interface Props {}
-export default class App extends Component<Props> {
+export default class App extends PureComponent<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Text style={styles.title}>Live commentary</Text>
+      </Container>
     );
   }
 }
@@ -30,14 +20,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  title: {
+    fontSize: 32,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    paddingBottom: 10,
   },
 });
